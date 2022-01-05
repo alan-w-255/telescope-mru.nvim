a408fa6b4489b4377daec3065def5f9c6af26015
wangyuanmou <wangyuanmou@zhuozhuo.io>
2022-01-05 20:51:31 +0800

    feat: add mru picker to telescope.

---
+++ lua/telescope/_extensions/mru.lua
@@ -0,0 +1,29 @@
+local pickers = require'telescope.pickers'
+local finders = require'telescope.finders'
+local conf = require'telescope.config'.values
+
+local MRUFiles = function (opts)
+    opts = opts or {}
+    local mrufiles = vim.fn["MruGetFiles"]()
+    local entries  = {}
+    for _, v in ipairs(mrufiles) do
+        local path = require'plenary.path':new(v)
+        if path:expand() ~= vim.api.nvim_buf_get_name(0) then
+            table.insert(entries, path:expand())
+        end
+    end
+    pickers.new(opts, {
+        prompt_title = "MRU files",
+        finder = finders.new_table {
+            results = entries,
+        },
+        sorter = conf.file_sorter(opts),
+        previewer = conf.file_previewer(opts),
+    }):find()
+end
+
+require'telescope'.register_extension({
+    exports = {
+        mru = MRUFiles,
+    }
+})
